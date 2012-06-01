6634971be5d7df7bf82fd5b0a8d0f18aeea3baf4
Georgy Gubsky <georgegubsky@gmail.com>
2012-06-02 05:54:12 +0700

    remove unused code

--- src/ru/gubsky/study/elib/models/ClientModel.java
+++ src/ru/gubsky/study/elib/models/ClientModel.java
@@ -19,60 +19,7 @@ public class ClientModel
     final static String HOST = "localhost";
     final static int PORT = 15755;
     final static int OPERATION_SEARCH = 1;
-    final static int OPERATION_ADD_VIEW = 2;
-    
-    public String[] getGenres()
-    {
-        String[] str = new String[] {
-            "новеллы",
-            "рассказы",
-            "компьютерная литература"
-        };
-        return str;
-    }
-
-    public String[] getAuthors()
-    {
-        String[] str = new String[] {
-            "гоша",
-            "иванов",
-            "николай иванович"
-        };
-        return str;
-    }
-
-    public ArrayList<Book> getPopularBook(int start, int end)
-    {
-        ArrayList<Book> bukz = new ArrayList<>();
-        for (int i = start; i <= end; i++) {
-            Book b1 = new Book();
-            b1.author = "gosha";
-            b1.date = new Date(1000303);
-            b1.genre = "genre";
-            b1.name = "как я вышел погулять";
-            b1.popularity = 10;
-            bukz.add(b1);
-        }
-        return bukz;
-    }
-    
-    public String getText(int bookId)
-    {
-        return "ajskdfjaksdjfk;ajsdfkjadfaksdjfkasjdfkjasdkfjaksdjfkajsdfkjasf"
-                + "aksdfjaksjdfklajsd;afjskdfjaksdfj"
-                + "askdfjak;dsjfk;asjdfkajsf"
-                + "askdfjaskfjaskfjakdfjakldfs"
-                + "afgaskgjaskgjaknkdfb"
-                + "dgaskgjaaskgjaaskgjaaskgjaaskgjaa"
-                + "gaskgjaaskgjaaskgjaaskgjaaskgjaa"
-                + "dsaskgjaaskgjaaskgjag"
-                + "asaskgjaaskgjaaskgjag"
-                + "adaskgjaaskgjaaskgjasg"
-                + "dsfgkdfjgkdjrgkdjgkd"
-                + "rgaskgjaaskgjaaskgjad"
-                + "gaskgjaaskgjaaskgjaaskgjaaskgja"
-                + "dgradjgkajsdgkajskdgjak;dgj";
-    }
+    final static int OPERATION_ADD_VIEW = 2;  
     
     public ArrayList<Book> getBooksBySearching(String text)
     {
